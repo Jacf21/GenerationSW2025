3050ce7034775debc340fc24ba63883d68621634
GUILDER <pplg39395@gmail.com>
2025-10-18 16:48:34 -0400

    Imoplementacion de envio de correo despues de registrarse

---
+++ Frontend/src/pages/Paneles/PanelEditor.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Paneles.css";
+
+const PanelEditor = () => {
+  return (
+    <div className="panel-container">
+      <h1>Panel del Profesor Editor</h1>
+      <div className="panel-content">
+        <p>Bienvenido al panel de profesor editor</p>
+        {/* Aquí irá el contenido específico del editor */}
+      </div>
+    </div>
+  );
+};
+
+export default PanelEditor;
