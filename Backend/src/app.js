ad949547e3f41eb08ef3496664ee1c0b28d54600
JAFETLP <jafetcana@gmail.com>
2025-11-30 18:49:25 -0400

    cambio en el archivo app.js pera obtener el cors del .env

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,11 @@ import matriculaRoutes from "./routes/matriculaRoutes.js";
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  process.env.CORS_ORIGIN,
+  "http://localhost:5173",
+  "http://localhost:5174",
+].filter(Boolean);
 
 app.use(
   cors({
